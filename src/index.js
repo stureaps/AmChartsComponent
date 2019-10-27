118175dd78d56e6b7f761fb8e94adfba522b6a9e
Jakob Kruse <kruse@kruse-net.dk>
2019-10-27 13:13:35 +0100

    Added gauge chart demo. Added ClockHandShowValue method. Added OnClick event for both slices and columns.

--- src/index.js
+++ src/index.js
@@ -2,6 +2,16 @@ import * as core from "@amcharts/amcharts4/core";
 import * as charts from "@amcharts/amcharts4/charts";
 import animated from "@amcharts/amcharts4/themes/animated";
 
+let decimalSeparator;
+
+function stringToNum(string) {
+    return df.sys.data.stringToNum(string, decimalSeparator);
+}
+
+function numToString(num, decimals) {
+    return df.sys.data.numToString(num, decimalSeparator, decimals);
+}
+
 core.useTheme(animated);
 
 class AmChartsComponent extends df.WebBaseControl {
@@ -9,6 +19,8 @@ class AmChartsComponent extends df.WebBaseControl {
         super(sName, oParent);
 
         this.event('OnClickSlice');
+
+        decimalSeparator = this.getWebApp().psDecimalSeparator;
 
         this._eChart = null;
     }
@@ -29,10 +41,17 @@ class AmChartsComponent extends df.WebBaseControl {
     }
 
     registerEventListeners(chart) {
-        // Here we can add all the vairous event listeners of the different chart types
-        if (chart instanceof charts.PieChart) {
+        // Here we can add all the various event listeners of the different chart types
+        if (chart instanceof charts.SerialChart) {
+            // console.log('is a serialchart');
             chart.series.each(series => {
-                series.slices.template.events.on('hit', event => this.onClickSlice(event));
+                if (series instanceof charts.ColumnSeries) {
+                    series.columns.template.events.on('hit', event => this.onClick('column', event.target));
+                }
+                if (series instanceof charts.PieSeries) {
+                    series.slices.template.events.on('hit', event => this.onClick('slice', event.target));
+                    series.slices.template.events.on('hit', event => this.onClickSlice(event));
+                }
             });
         }
     }
@@ -50,6 +69,33 @@ class AmChartsComponent extends df.WebBaseControl {
         this.sizeHeight(-1);
     }
 
+    clockHandShowValue(index, value, duration) {
+        if (typeof index === 'string') {
+            index = parseInt(index);
+        }
+        if (typeof value === 'string') {
+            value = stringToNum(value);
+        }
+        if (typeof duration === 'string') {
+            duration = parseInt(duration);
+        }
+        if (this._eChart instanceof charts.GaugeChart && this._eChart.hands.hasIndex(index)) {
+            this._eChart.hands.getIndex(index).showValue(value, duration);
+        }
+    }
+
+    onClick(type, target) {
+        if (target.dataItem instanceof charts.ColumnSeriesDataItem) {
+            // console.log('is a column');
+            this.fire('OnClick', [type, target.dataItem.categoryX, target.dataItem.categoryY]);
+        }
+        if (target.dataItem instanceof charts.PieSeriesDataItem) {
+            // console.log('is a slice');
+            this.fire('OnClick', [type, target.dataItem.category]);
+        }
+    }
+
+    // Deprecated
     onClickSlice(event) {
         this.fire('OnClickSlice', [event.target.dataItem.category]);
     }
